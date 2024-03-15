8b97e8a76fd0b8488e98a57505c5b3e30f6a7b9a
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-03-15 12:40:53 -0500

    create porfolio

---
+++ src/assets/data.ts
@@ -0,0 +1,18 @@
+import type { PageInterface, SocialInterface } from "../entities/social.interface";
+
+export const social: SocialInterface[] = [
+  {
+    logo: "bx bxl-github",
+  },
+  {
+    logo: "bx bxl-linkedin",
+  },
+];
+
+
+export const info:PageInterface[] =[
+  
+    {text:'About'},
+    {text:'Proyects',}
+  
+]
---
+++ src/entities/social.interface.ts
@@ -0,0 +1,6 @@
+export  interface SocialInterface{
+    logo:string
+}
+export  interface PageInterface{
+    text:string
+}
